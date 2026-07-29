d6497da55aa21f8f86ed04bc74bac4bb1022ede2
gthanopoulos <geor.thanopoulos@gmail.com>
2026-07-29 20:07:09 +0300

    "feat: checks"

--- src/main/java/gr/aueb/cf/schoolapp/repository/TeacherRepository.java
+++ src/main/java/gr/aueb/cf/schoolapp/repository/TeacherRepository.java
@@ -10,8 +10,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 import java.util.UUID;
 
-@Repository
-// If we don't "extends" the JpaRepository for some reason, then the @Repository is Obligatory! SOS!
+@Repository                                                                // If we don't "extends" the JpaRepository for some reason, then the @Repository is Obligatory! SOS!
 public interface TeacherRepository extends JpaRepository<Teacher, Long> {
 
     Optional<Teacher> findByVat(String vat);                // In order to avoid NullPointerException in case the vat does not exist.
